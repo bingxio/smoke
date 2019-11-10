022aa7ab053b157e6509a7b5d8ed1f763f6deee5
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-11-10 19:27:26 +0800

    Update smoke-interpreter.rs

--- smoke-interpreter.rs
+++ smoke-interpreter.rs
@@ -16,7 +16,7 @@ impl Token {
   }
 }
 
-fn tokenizer(mut chars: Chars) -> Vec<Token> {
+fn tokenizer(mut chars: Chars) -> (bool, Vec<Token>) {
   let mut tokens: Vec<Token> = Vec::new();
   let mut line = 1;
 
@@ -47,12 +47,12 @@ fn tokenizer(mut chars: Chars) -> Vec<Token> {
       ch @ '!' => tokens.push(Token::new(T, ch, line)),
       ch @ _ => {
         eprintln!("SyntaxErr: unknown char: {}", ch);
-        std::process::exit(1);
+        return (false, tokens);
       }
     }
   }
 
-  tokens
+  (true, tokens)
 }
 
 struct Chunk {
@@ -61,7 +61,9 @@ struct Chunk {
   memory: [i32; 10000],
   // The position of every layer in memory.
   // 0 -> 100
-  p: i32
+  p: i32,
+  // If running in repl mode to do something.
+  repl_mode: bool
 }
 
 impl Display for Chunk {
@@ -99,7 +101,11 @@ impl Chunk {
 
         }
         D => {
-          print!("{}", self.memory[self.p as usize] as u8 as char);
+          if self.repl_mode {
+            println!("{}", self.memory[self.p as usize] as u8 as char);
+          } else {
+            print!("{}", self.memory[self.p as usize] as u8 as char);
+          }
         },
         C => {
 
@@ -150,15 +156,51 @@ fn run_file(path: String) {
 
     file.read_to_string(&mut contents).expect("Cannot read buffer to String.");
 
-    Chunk { tokens: tokenizer(contents.chars()), memory: [0; 10000], p: 0 }.execute();
+    Chunk { tokens: tokenizer(contents.chars()).1, memory: [0; 10000], p: 0, repl_mode: true }.execute();
   }
 }
 
 fn run_repl() {
-  println!("The smoke programming language interpreter [ version 1.0.0 ]");
+  println!("Smoke 1.0.0 [dev, Nov 10 2019, 17:18] Nice to meet you >_ !");
+  println!("Type \"help\", \"copyright\", \"license\" for more information.");
 
   let mut memory: [i32; 10000] = [0; 10000];
   let mut p: i32 = 0;
+
+  fn input_help() {
+    let help = "
+      Welcome to Smoke 1.0's help utility !
+    ";
+    println!("{}", help);
+  }
+
+  fn input_copyright() {
+    let license = "
+      Copyright [2019] [Turaiiao]
+
+      Licensed under the Apache License, Version 2.0 (the \"License\");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an \"AS IS\" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+      ";
+    println!("{}", license);
+  }
+
+  fn input_license() {
+    let license = "
+      The Smoke is open source ! More information to type \"copyright\".
+
+      You can star this project on GitHub: https://github.com/turaiiao/smoke
+    ";
+    println!("{}", license);
+  }
 
   loop {
     print!(">>> ");
@@ -169,11 +211,23 @@ fn run_repl() {
     std::io::stdin().read_line(&mut line).expect("Failed to read line !");
 
     if line.trim_end().len() > 0 {
-      let mut chunk = Chunk { tokens: tokenizer(line.chars()), memory, p };
+      match line.as_str().trim_end() {
+        "help"      => input_help(),
+        "copyright" => input_copyright(),
+        "license"   => input_license(),
+        "exit"      => break,
+        _=> {
+          let tokens = tokenizer(line.chars());
 
-      if chunk.execute() {
-        memory = chunk.memory;
-        p = chunk.p;
+          if tokens.0 {
+            let mut chunk = Chunk { tokens: tokens.1, memory, p, repl_mode: true };
+
+            if chunk.execute() {
+              memory = chunk.memory;
+              p = chunk.p;
+            }
+          }
+        }
       }
     }
   }
