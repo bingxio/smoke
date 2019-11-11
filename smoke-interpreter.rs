41c749affc347ce626c0a0c7c614f3cf5a0be14c
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-11-11 13:09:24 +0800

    Update smoke-interpreter.rs

--- smoke-interpreter.rs
+++ smoke-interpreter.rs
@@ -1,18 +1,24 @@
-use std::io::{Write, Read};
+use std::io::{Write, Read, stdin, stdout};
 use std::fs::File;
-use std::fmt::{Display, Formatter, Error, Debug};
+use std::fmt::Debug;
 use std::str::Chars;
+use std::mem::discriminant;
+
 use crate::TypeDef::*;
 
 #[derive(Debug)]
-enum TypeDef { L, R, A, M, LB, RB, D, C, T }
+enum TypeDef { L, R, A, M, LB, RB, D, S, C, T }
+
+fn typedef_eq(a: &TypeDef, b: &TypeDef) -> bool {
+  discriminant(a) == discriminant(b)
+}
 
 #[derive(Debug)]
-struct Token { typedef: TypeDef, literal: char, line: i32 }
+struct Token { typedef: TypeDef, line: i32 }
 
 impl Token {
-  fn new(typedef: TypeDef, literal: char, line: i32) -> Self {
-    Token { typedef, literal, line }
+  fn new(typedef: TypeDef, line: i32) -> Self {
+    Token { typedef, line }
   }
 }
 
@@ -36,15 +42,16 @@ fn tokenizer(mut chars: Chars) -> (bool, Vec<Token>) {
           }
         }
       }
-      ch @ '<' => tokens.push(Token::new(L, ch, line)),
-      ch @ '>' => tokens.push(Token::new(R, ch, line)),
-      ch @ '+' => tokens.push(Token::new(A, ch, line)),
-      ch @ '-' => tokens.push(Token::new(M, ch, line)),
-      ch @ '[' => tokens.push(Token::new(LB, ch, line)),
-      ch @ ']' => tokens.push(Token::new(RB, ch, line)),
-      ch @ '.' => tokens.push(Token::new(D, ch, line)),
-      ch @ ',' => tokens.push(Token::new(C, ch, line)),
-      ch @ '!' => tokens.push(Token::new(T, ch, line)),
+      '<' => tokens.push(Token::new(L, line)),
+      '>' => tokens.push(Token::new(R, line)),
+      '+' => tokens.push(Token::new(A, line)),
+      '-' => tokens.push(Token::new(M, line)),
+      '[' => tokens.push(Token::new(LB, line)),
+      ']' => tokens.push(Token::new(RB, line)),
+      '.' => tokens.push(Token::new(D, line)),
+      ',' => tokens.push(Token::new(C, line)),
+      '!' => tokens.push(Token::new(T, line)),
+      '*' => tokens.push(Token::new(S, line)),
       ch @ _ => {
         eprintln!("SyntaxErr: unknown char: {}", ch);
         return (false, tokens);
@@ -60,89 +67,130 @@ struct Chunk {
   // memory -> 10000 * i32
   memory: [i32; 10000],
   // The position of every layer in memory.
-  // 0 -> 100
+  // 0 -> 10000
   p: i32,
+  // Index of the token list range.
+  pos: i32,
   // If running in repl mode to do something.
   repl_mode: bool
 }
 
-impl Display for Chunk {
-  fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-    write!(f, "Chunk -> [ memory size: {} ]", self.memory.len())
-  }
-}
-
 impl Chunk {
   fn execute(&mut self) -> bool {
-    let mut position = 0;
-
-    loop {
-      if position == self.tokens.len() {
-        break;
+    while let Some(token) = self.tokens.get(self.pos as usize) {
+      match token.typedef {
+        L =>  self.l_stmt(),
+        R =>  self.r_stmt(),
+        A =>  self.a_stmt(),
+        M =>  self.m_stmt(),
+        LB => self.lb_stmt(),
+        RB => self.rb_stmt(),
+        D =>  self.d_stmt(),
+        S =>  self.s_stmt(),
+        C =>  self.c_stmt(),
+        T =>  self.t_stmt(),
       }
-
-      let ins = self.tokens.get(position).unwrap();
-
-      match ins.typedef {
-        L => {
-          if self.p != 0 {
-            self.p -= 1;
-          }
-        }
-        R => {
-          self.p += 1;
-        },
-        A => self.memory[self.p as usize] += 1,
-        M => self.memory[self.p as usize] -= 1,
-        LB => {
-
-        }
-        RB => {
-
-        }
-        D => {
-          if self.repl_mode {
-            println!("{}", self.memory[self.p as usize] as u8 as char);
-          } else {
-            print!("{}", self.memory[self.p as usize] as u8 as char);
-          }
-        },
-        C => {
-
-        },
-        T => {
-          let mut skip = 50;
-
-          for (i, v) in self.memory.iter().enumerate() {
-            print!("{} ", v);
-
-            if i + 1 % 50 == 0 {
-              println!();
-            }
-
-            if i == skip {
-              println!("\nPress `n` to next and press other to quit.");
-
-              let mut line = String::new();
-
-              std::io::stdin().read_line(&mut line).expect("Failed to read line !");
-
-              if line.eq(&"n\r\n".to_string()) {
-                skip += 50;
-                continue;
-              } else {
-                break;
-              }
-            }
-          }
-          println!();
-        }
-      }
-
-      position += 1;
+      self.pos += 1;
     }
 
     true
+  }
+
+  fn get_memory_value(&self, pos: i32) -> i32 {
+    self.memory[pos as usize]
+  }
+
+  fn l_stmt(&mut self) {
+    if self.p != 0 {
+      self.p -= 1;
+    }
+  }
+
+  fn r_stmt(&mut self) {
+    self.p += 1;
+  }
+
+  fn a_stmt(&mut self) {
+    self.memory[self.p as usize] += 1;
+  }
+
+  fn m_stmt(&mut self) {
+    self.memory[self.p as usize] -= 1;
+  }
+
+  fn lb_stmt(&mut self) {
+    let p_backup = self.p;
+
+    self.pos += 1;
+
+    loop {
+      if self.pos as usize == self.tokens.len() {
+        eprintln!("SyntaxErr: expect right bracket that program was end.");
+        std::process::exit(1);
+      }
+
+      let tok = self.tokens.get(self.pos as usize).unwrap();
+
+      if typedef_eq(&tok.typedef, &RB) {
+        self.pos += 1;
+        break;
+      }
+
+      if self.get_memory_value(p_backup) == 0 {
+        break;
+      }
+
+      self.execute();
+    }
+  }
+
+  fn rb_stmt(&mut self) {}
+
+  fn d_stmt(&mut self) {
+    print!("{}{}", self.memory[self.p as usize] as u8 as char, if self.repl_mode {
+      '\n'
+    } else {
+      ' '
+    });
+  }
+
+  fn s_stmt(&mut self) {
+    print!("{}{}", self.memory[self.p as usize], if self.repl_mode {
+      '\n'
+    } else {
+      ' '
+    })
+  }
+
+  fn c_stmt(&mut self) {
+
+  }
+
+  fn t_stmt(&mut self) {
+    let mut skip = 50;
+
+    for (i, v) in self.memory.iter().enumerate() {
+      print!("{} ", v);
+
+      if i + 1 % 50 == 0 {
+        println!();
+      }
+
+      if i == skip {
+        println!("\nPress \"n\" to next and press other to quit.");
+
+        let mut line = String::new();
+
+        stdin().read_line(&mut line).expect("Failed to read line !");
+
+        if line.trim_end().eq(&"n".to_string()) {
+          skip += 50;
+          continue;
+        } else {
+          break;
+        }
+      }
+    }
   }
 }
 
@@ -156,12 +204,18 @@ fn run_file(path: String) {
 
     file.read_to_string(&mut contents).expect("Cannot read buffer to String.");
 
-    Chunk { tokens: tokenizer(contents.chars()).1, memory: [0; 10000], p: 0, repl_mode: true }.execute();
+    Chunk {
+      tokens: tokenizer(contents.chars()).1,
+      memory: [0; 10000],
+      p: 0,
+      pos: 0,
+      repl_mode: true
+    }.execute();
   }
 }
 
 fn run_repl() {
-  println!("Smoke 1.0.0 [dev, Nov 10 2019, 17:18] Nice to meet you >_ !");
+  println!("Smoke 1.0.0 [dev, Nov 10 2019, 17:18] Hello, nice to meet you >_ !");
   println!("Type \"help\", \"copyright\", \"license\" for more information.");
 
   let mut memory: [i32; 10000] = [0; 10000];
@@ -207,8 +261,8 @@ fn run_repl() {
 
     let mut line = String::new();
 
-    std::io::stdout().flush().expect("Failed to flush the screen !");
-    std::io::stdin().read_line(&mut line).expect("Failed to read line !");
+    stdout().flush().expect("Failed to flush the screen !");
+    stdin().read_line(&mut line).expect("Failed to read line !");
 
     if line.trim_end().len() > 0 {
       match line.as_str().trim_end() {
@@ -220,7 +274,7 @@ fn run_repl() {
           let tokens = tokenizer(line.chars());
 
           if tokens.0 {
-            let mut chunk = Chunk { tokens: tokens.1, memory, p, repl_mode: true };
+            let mut chunk = Chunk { tokens: tokens.1, memory, p, pos: 0, repl_mode: true };
 
             if chunk.execute() {
               memory = chunk.memory;
